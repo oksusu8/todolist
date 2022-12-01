d723e682033699545b8c7c8efec3fa7321db60ee
jhson929 <sjh05109@gmail.com>
2022-12-01 23:02:26 +0900

    [FEAT] express 기본 세팅, 할일 추가/삭제

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// 몽고DB 연결
+mongoose.connect("mongodb://127.0.0.1/todo_express", {
+  useNewUrlParser:true,
+  useUnifiedTopology: true,
+});
+
+// 미들웨어 설정
+app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.use(require("./routes/home"));
+app.use(require("./routes/todo"));
+
+// 서버 설정
+app.listen(3000, () => {
+  console.log("Server started listening on port : 3000");
+});
